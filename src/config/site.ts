e1e0c31530d5ce3314ef33c8872f6fd69973d7e0
Davi Lacerda <dlacerda9@gmail.com>
2023-07-14 02:41:42 -0300

    docs: add siteConfig

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+    name: "Davi Lacerda",
+    url: "https://davilacerda.com",
+    description:
+        "Davi Lacerda is a developer with knowledge in Next.js, React, TypeScript, TailwindCSS, and more. Currently undergraduate student in Computer Science at the Federal University of Uberlândia(UFU).",
+    links: {
+        github: "https://github.com/davilacerda",
+        linkedin: "https://www.linkedin.com/in/davi16lacerda/",
+        instagram: "https://www.instagram.com/davi16lacerda/",
+        email: "mailto:davi16lacerda@gmail.com",
+    },
+};
+
+export type SiteConfig = typeof siteConfig;
