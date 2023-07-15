7efbdd2533333e95f74c85d5757c1640f43b6158
Davi Lacerda <dlacerda9@gmail.com>
2023-07-15 10:58:41 -0300

    docs: description supports multiple languages

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,10 @@
 export const siteConfig = {
     name: "Davi Lacerda",
     url: "https://davilacerda.com",
-    description:
-        "Davi Lacerda is a developer with knowledge in Next.js, React, TypeScript, TailwindCSS, and more. Currently undergraduate student in Computer Science at the Federal University of Uberlândia(UFU).",
+    description: {
+        en: "Davi Lacerda is a developer with knowledge in Next.js, React, TypeScript, TailwindCSS, and more. Currently undergraduate student in Computer Science at the Federal University of Uberlândia(UFU).",
+        "pt-br": "Davi Lacerda é um desenvolvedor com conhecimentos em Next.js, React, TypeScript, TailwindCSS, e mais. Atualmente estudante de graduação em Ciência da Computação na Universidade Federal de Uberlândia(UFU).",
+    },
     links: {
         github: "https://github.com/davilacerda",
         linkedin: "https://www.linkedin.com/in/davi16lacerda/",
