93004a90b996e8bbd06dc83253cc7d67f282177d
Davi Lacerda <dlacerda9@gmail.com>
2023-07-15 13:37:31 -0300

    build: add brand color and update container

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
             center: true,
             padding: "2rem",
             screens: {
-                "2xl": "1400px",
+                "2xl": "768px",
             },
         },
         extend: {
@@ -49,6 +49,19 @@ module.exports = {
                 card: {
                     DEFAULT: "hsl(var(--card))",
                     foreground: "hsl(var(--card-foreground))",
+                },
+                brand: {
+                    DEFAULT: "#4c34d8",
+                    50: "#eef2ff",
+                    100: "#e3e8ff",
+                    200: "#ccd4ff",
+                    300: "#abb7ff",
+                    400: "#898eff",
+                    500: "#6c66fc",
+                    600: "#5843f2",
+                    700: "#4c34d8",
+                    800: "#3d2ea8",
+                    900: "#342d80",
                 },
             },
             borderRadius: {
