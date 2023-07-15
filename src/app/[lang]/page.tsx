8062e1bbf03b57d3173250821c8f2acf9bcef9a7
Davi Lacerda <dlacerda9@gmail.com>
2023-07-15 11:26:02 -0300

    refactor: type Props

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,16 +1,16 @@
-import { getDictionary } from '@/i18n/get-dictionary';
-import { Locale } from '@/i18n/i18n-config';
+import { siteConfig } from "@/config/site";
+import { getDictionary } from "@/i18n/get-dictionary";
+import { Locale } from "@/i18n/i18n-config";
 
-export default async function Home({
-  params: { lang },
-}: {
-  params: { lang: Locale };
-}) {
-  const dictionary = await getDictionary(lang);
+type Props = {
+    params: {
+        lang: Locale;
+    };
+};
 
-  return (
-    <span>
-      {dictionary['home'].hello}
-    </span>
-  )
+export default async function Home({ params }: Props) {
+    const { lang } = params;
+    const dictionary = await getDictionary(lang);
+
+    return <span>{dictionary["home"].hello}</span>;
 }
